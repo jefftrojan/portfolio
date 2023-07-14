46763270c1f7059fcec8ba8b2f4ee5d9719bd66d
jefftrojan <j.dauda@alustudent.com>
2023-07-14 07:11:07 +0200

    Added site layout

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navbar">
+        <span className="logo">Portfolio</span>
+        <ul className="nav-links">
+          <li id='#section1'><a href="#section1">Section 1</a></li>
+          <li><a href="#section2">Section 2</a></li>
+          <li><a href="#section3">Section 3</a></li>
+        </ul>
+      </nav>
+      <section className="section section1">
+        <h1>Hero Section</h1>
+      </section>
+      <section className="section section2">
+        <h1>Projects</h1>
+      </section>
+      <section className="section section3">
+        <h1>Experience</h1>
+      </section>
+      <section className="section section3">
+        <h1>Contact</h1>
+      </section>
     </div>
   );
 }
