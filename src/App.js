fae209e6adc400bb69b9a648a4b357387f47ab33
jefftrojan <j.dauda@alustudent.com>
2023-07-14 08:09:00 +0200

    Added site layout

--- src/App.js
+++ src/App.js
@@ -2,28 +2,31 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const scrollToSection = (sectionId) => {
+    const section = document.getElementById(sectionId);
+    section.scrollIntoView({ behavior: 'smooth' });
+  };
   return (
     <div className="App">
       <nav className="navbar">
-        <span className="logo">Portfolio</span>
+        <span className="logo"><u>JDK</u></span>
         <ul className="nav-links">
-          <li id='#section1'><a href="#section1">Section 1</a></li>
-          <li><a href="#section2">Section 2</a></li>
-          <li><a href="#section3">Section 3</a></li>
+          <li id='#about'><a href="#about">About</a></li>
+          <li><a href="#projects">Projects</a></li>
+          <li><a href="#contact">Contact</a></li>
         </ul>
       </nav>
-      <section className="section section1">
-        <h1>Hero Section</h1>
+      <section className="section about">
+        <h1>About</h1>
+       
       </section>
-      <section className="section section2">
+      <section className="section projects">
         <h1>Projects</h1>
       </section>
-      <section className="section section3">
-        <h1>Experience</h1>
-      </section>
-      <section className="section section3">
+      <section className="section contact">
         <h1>Contact</h1>
       </section>
+      
     </div>
   );
 }
